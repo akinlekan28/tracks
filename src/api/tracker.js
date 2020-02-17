b186b2eb88a638766fc6d86a34f286b8436debd9
akintola <akinlekan28@yahoo.com>
2020-02-17 09:46:14 +0100

    updated server endpoint

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://akinlekan28-track-server.glitch.me"
 });
 
 instance.interceptors.request.use(
